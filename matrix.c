3f526d116628eddf5b1bd474df6b53ca93bfc501
임정주 <41723716+CraftJJGamer308@users.noreply.github.com>
2024-02-15 20:01:21 +0100

    Float-Support, Verschönerung, Optimierung

--- matrix.c
+++ matrix.c
@@ -1,26 +1,28 @@
 // Craft JJ Industries
 // Determinant Calculator
-// Created on: 12 FEB 2024
+// Created on: 15 FEB 2024
 
 #include <stdio.h>
 #include <stdlib.h>
+#define PRINT_BAR (printf("\n-----------------------------\n"))
 
-int isEven(int i) { return i % 2 == 0 ? 1 : -1; }
+float isEven(int i) { return i % 2 == 0 ? 1. : -1.; }
 
-int det(int **a, int n) {
-  int s = 0, i, j, k;
-
-  // reserve space for (n-1)*(n-1) minor matrix
-  int **a1 = malloc((n - 1) * sizeof(int *));
-
-  // (n-1)*int matrix for each column
-  for (i = 0; i < n - 1; i++) {
-    a1[i] = malloc((n - 1) * sizeof(int));
-  }
-
+float det(float **a, int n) {
   // recursion break condition
   if (n == 1) {
     return a[0][0];
+  }
+
+  int i, j, k;
+  float s = 0;
+
+  // reserve space for (n-1)*(n-1) minor matrix
+  float **a1 = malloc((n - 1) * sizeof(float *));
+
+  // (n-1)*int for each column
+  for (i = 0; i < n - 1; i++) {
+    a1[i] = malloc((n - 1) * sizeof(float));
   }
 
   // recursion using Laplace-Algorithm
@@ -45,33 +47,40 @@ int det(int **a, int n) {
 
 int main() {
   int n, i, j;
+  printf("Craft JJ Industries, "
+         "2024\nMatrix-Determinante-Rechner");
+  PRINT_BAR;
+
   printf("Matrixgröße eingeben: ");
   scanf("%d", &n);
 
   // reserve space for n*n matrix
-  int **a = malloc(n * sizeof(int *));
+  float **a = malloc(n * sizeof(float *));
 
   // n*int for each column
   for (i = 0; i < n; i++) {
-    a[i] = malloc(n * sizeof(int));
+    a[i] = malloc(n * sizeof(float));
   }
 
   // input
   for (i = 0; i < n; i++) {
     printf("Zeile %d: ", i + 1);
     for (j = 0; j < n; j++) {
-      scanf("%d", &a[i][j]);
+      scanf("%f", &a[i][j]);
     }
   }
 
   // print matrix
+  PRINT_BAR;
+  printf("eingegebene Matrix: \n");
   for (i = 0; i < n; i++) {
-    printf("\n");
     for (j = 0; j < n; j++) {
-      printf("%5d", a[i][j]);
+      printf("%7.2f", a[i][j]);
     }
+    printf("\n");
   }
 
   // print result
-  printf("\n\nDeterminante: %d\n", det(a, n));
+  PRINT_BAR;
+  printf("Determinante: %f\n", det(a, n));
 }
